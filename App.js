6a18bf31af3363413d582693a2aa88793ca1f0f5
tundeiness <tundeiness@yahoo.co.uk>
2020-06-26 16:27:02 +0100

    WIP App.js

--- App.js
+++ App.js
@@ -1,4 +1,23 @@
-const express = require('express')
-const bodyParser = require('bodyParser')
-const cors = require('cors')
-const sendGrid = require('@sendgrid/mail')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sendGrid = require('@sendgrid/mail');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,  DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next();
+})
+
+app.get('/api', (req, res, next) =>{
+  res.send('API Status: Running')
+})
+
+
+app.listen(4040, '0.0.0.0');
